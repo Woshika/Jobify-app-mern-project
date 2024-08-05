ae5a1daa9b5ae4eb1b750e7b82ad2ffba0898a0e
Woshika <wkavindi016@gmail.com>
2024-08-05 07:13:05 -1200

    Admin Page Completed

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -3,10 +3,11 @@ import { useLoaderData, redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
+import { StatItem } from "../components";
 
 export const loader = async () => {
   try {
-    const response = await customFetch.get("/users/admin/app-status");
+    const response = await customFetch.get("/users/admin/app-stats");
     return response.data;
   } catch (error) {
     toast.error("You are not authorized to view this page");
@@ -18,7 +19,20 @@ const Admin = () => {
   const { users, jobs } = useLoaderData();
   return (
     <Wrapper>
-      <h1>admin page</h1>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8fg"
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   );
 };
