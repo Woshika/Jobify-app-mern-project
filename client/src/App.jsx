483f93073665c0ce3fd17f5a3b3310540f005a54
Woshika <wkavindi016@gmail.com>
2024-08-05 15:00:36 -1200

    Profile Action

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ import { action as editJobAction } from "./pages/EditJob";
 import { loader as editJobsLoader } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as adminLoader } from "./pages/Admin";
+import { action as profileAction } from "./pages/Profile";
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -75,6 +76,7 @@ const router = createBrowserRouter([
           {
             path: "profile",
             element: <Profile />,
+            action: profileAction,
           },
           {
             path: "admin",
