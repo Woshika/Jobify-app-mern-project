929455c2df520f1f9ea04f81f8988728af3890f5
Woshika <wkavindi016@gmail.com>
2024-08-06 10:18:53 -1200

    Cloudinary

--- server.js
+++ server.js
@@ -2,26 +2,33 @@ import "express-async-errors";
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+const app = express();
 import morgan from "morgan"; // Ensure you have installed morgan
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+
 // routers
 import JobRouter from "./routes/JobRouters.js";
 import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
-const app = express();
+
 //public
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
+
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
-import cookieParser from "cookie-parser";
-// Middleware to parse JSON bodies
-app.use(express.json());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
