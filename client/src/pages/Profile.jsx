76b9c97b2ece20fa5c00d3789d1a59e3848c9e25
Woshika <wkavindi016@gmail.com>
2024-08-05 14:35:52 -1200

    Profile Page Structure

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { FormRows } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { useNavigation, Form } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
-function Profile() {
-  return <h1>Profile Page</h1>;
-}
+const Profile = () => {
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlFor="avatar" className="form-label">
+              Select an image file (max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              className="form-input"
+              accept="image/*"
+            />
+          </div>
+          {/* file input */}
+          <FormRows type="text" name="name" defaultValue={name} />
+          <FormRows
+            type="text"
+            name="lastName"
+            labelText="last name"
+            defaultValue={lastName}
+          />
+          <FormRows type="email" name="email" defaultValue={email} />
+          <FormRows type="text" name="location" defaultValue={location} />
+          <button
+            className="btn btn-block form-btn"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting..." : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
 
 export default Profile;
